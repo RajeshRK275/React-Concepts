8f2f799ccaa77953d6590a97b36aab56f8c69c4e
RajeshRk275 <rajesheswaran27599@gmail.com>
2024-09-27 18:35:23 +0530

    Use Reducer Example Coded

--- Hooks/Use Reducer/usereducer/src/App.js
+++ Hooks/Use Reducer/usereducer/src/App.js
@@ -1,9 +1,43 @@
-import './App.css';
+import { act, useReducer, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [state, dispatch] = useReducer(reducer, 0);
+
+  function reducer(state, action) {
+    if (action.type === "Increment") {
+      return state + 1;
+    }
+    if (action.type === "Decrement") {
+      return state - 1;
+    }
+  }
+
   return (
     <div className="App">
-      Use Reducer App
+      {/* This part is the Example of useState */}
+      <h3>{value}</h3>
+      <button onClick={() => setValue(value + 1)}>Increment</button>
+      <button onClick={() => setValue(value - 1)}>Decrement</button>
+      <br />
+
+      {/* This part is the Example of useReducer */}
+      <button
+        onClick={() => {
+          dispatch({ type: "Increment" });
+        }}
+      >
+        Add
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "Decrement" });
+        }}
+      >
+        Subtract
+      </button>
+      <h3>{state}</h3>
     </div>
   );
 }
